75e89113bf5526b16f77c5e8ac233b711bf60493
edwingmo <monrroyedwing@gmail.com>
2022-05-12 19:07:40 -0400

    probando statics

--- irsmain/urls.py
+++ irsmain/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
 from decouple import config
 
 if config('DEBUG'):
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
