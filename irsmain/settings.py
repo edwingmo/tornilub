387acd69c866751a029e1aa69c3986dda4291120
edwingmo <monrroyedwing@gmail.com>
2022-05-12 16:50:52 -0400

    carpeta tornilubfc

--- irsmain/settings.py
+++ irsmain/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = [config('ALLOWHOST')]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -95,7 +95,7 @@ if DEBUG == False:
             default=config('DATABASE_URL')
             )
     }
-    
+
 else:
     DATABASES = {
         'default': {
