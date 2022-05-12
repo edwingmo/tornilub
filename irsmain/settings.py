da1b85225c81479d3b7fa581221f29d3a1b966ad
edwingmo <monrroyedwing@gmail.com>
2022-05-12 12:47:48 -0400

    host

--- irsmain/settings.py
+++ irsmain/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://tornilub.herokuapp.com/']
+ALLOWED_HOSTS = ['tornilub.herokuapp.com']
 
 
 # Application definition
