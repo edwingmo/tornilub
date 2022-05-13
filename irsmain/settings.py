eb539ce9aa44c55301e811ce46e891cb518f49c9
edwingmo <monrroyedwing@gmail.com>
2022-05-13 15:18:59 -0400

    awss

--- irsmain/settings.py
+++ irsmain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["compresoresv.eba-hfxpxnuu.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
