ca59998a117d6c710f2aa2b1c09e77490e28acb9
edwingmo <monrroyedwing@gmail.com>
2022-05-17 12:48:45 -0400

    statics

--- irsmain/settings.py
+++ irsmain/settings.py
@@ -164,7 +164,7 @@ if probando == True:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
-    AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_FILE_OVERWRITE = True
     AWS_DEFAULT_ACL = 'public-read'
     AWS_LOCATION = 'static'
 
